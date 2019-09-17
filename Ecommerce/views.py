67a536d49f769dad3038e74f2e48e2c939bb1c40
sise76@gmail.com <sise76@gmail.com>
2019-09-17 22:32:10 +0200

    loginDecorator

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -6,8 +6,10 @@ from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from django.db.models import Sum, Count
+from django.contrib.auth.decorators import login_required
 
 #Menu
+@login_required
 def totalClientes(request):
     cliente_total=Clientes.objects.all()
     total_earning=Invoice.objects.all().aggregate(Sum('total')).get('total__sum', 0.00)
@@ -26,6 +28,7 @@ def totalClientes(request):
         'lowStock': lowStock
     })
 
+@login_required
 def TopFiveProducts(request):
     topFive=Productos.objects.extra(select={'a': 'nameproduct'}).values('a')\
         .annotate(available=Sum('invoiceitems__totalItem')).order_by('-available')[:5]
@@ -33,7 +36,7 @@ def TopFiveProducts(request):
         'topFive': topFive
     })
 
-
+@login_required
 def TopFiveClients(request):
     topFiveClients=Clientes.objects.extra(select={'a': 'name'}).values('a')\
         .annotate(available=Sum('invoice__total')).order_by('-available')[:5]
@@ -43,6 +46,7 @@ def TopFiveClients(request):
 
 
 #Client List
+@login_required
 def ClientesList(request):
     cliente_list=Clientes.objects.all()
     return render(request, 'Ecommerce/listaClientes.html', {'cliente_list': cliente_list})
@@ -54,11 +58,13 @@ class PostsClientes(ModelForm):
         fieldsClientes = ['name', 'apellido', 'phone', 'email', 'address']
 
 #Invoices per client
+@login_required
 def ClientInvoices(request, id):
     invoice_list=Invoice.objects.filter(clientes=id)
     return render(request, 'Ecommerce/listaInvoice.html', {'invoice_list': invoice_list})
 
 #Productos
+@login_required
 def ProductosList(request):
     productos_list=Productos.objects.all()
     return render(request, 'Ecommerce/listaProductos.html', {'productos_list': productos_list})
@@ -68,6 +74,7 @@ class PostsProducts(ModelForm):
         model = Productos
         fields = ['brand', 'nameproduct', 'description', 'category', 'price', 'photo', 'stock']
 
+@login_required
 def ProductDelete(request, id):
     print (id)
     post = get_object_or_404(Productos, id=id)
@@ -75,7 +82,7 @@ def ProductDelete(request, id):
     #messages.add_message(request, messages.SUCCESS, 'Product successfully deleted.')
     return redirect ('viewProducts')
 
-    
+@login_required    
 def ProductUpdate(request, id): 
     product = get_object_or_404(Productos, id=id)
     form = PostsProducts(request.POST or None, request.FILES or None, instance=product)
@@ -84,7 +91,7 @@ def ProductUpdate(request, id):
         return redirect ('viewProducts')
     return render(request, 'Ecommerce/ProductForm.html', {'form': form})
 
-
+@login_required
 def ProductCreate(request):
     form = PostsProducts(request.POST, request.FILES)
     if form.is_valid():
@@ -92,13 +99,14 @@ def ProductCreate(request):
         return redirect ('viewProducts')
     return render(request, 'Ecommerce/newProduct.html', {'form': form})
 
-
+@login_required
 def ProductInvoices(request, id):
     invoice_list=Invoice.objects.filter(productos=id)
     return render(request, 'Ecommerce/listaInvoice.html', {'invoice_list': invoice_list})
 
 
 #Invoice
+@login_required
 def InvoiceList(request):
     invoice_list=Invoice.objects.all()
     return render(request, 'Ecommerce/listaInvoice.html', 
